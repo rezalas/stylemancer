566eb49b4eb4de89f40020f8dc183e0f5aeca7e4
Matt Bear <hi@matt.am>
2019-10-06 15:25:05 +1100

    add triggers and travel between levels

--- public/game/game.js
+++ public/game/game.js
@@ -7,58 +7,126 @@
   window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-console.log(innerWidth);
-console.log(innerHeight * 0.67);
+let canvas,
+  hud = document.getElementById("hud"),
+  ctx,
+  width,
+  height,
+  lastWidth,
+  lastHeight,
+  friction,
+  gravity,
+  level = 0,
+  player = {},
+  keys = [],
+  boxes = [],
+  triggers = [];
 
-var canvas = document.getElementById("canvas"),
-  ctx = canvas.getContext("2d"),
-  width = window.innerWidth,
-  height = window.innerHeight * 0.67,
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+
+player.x = canvas.width / 10;
+player.y = canvas.height * 0.01;
+
+lastWidth = canvas.width;
+lsatHeight = canvas.height;
+
+// Initialise last width and height to correct canvas dimensions
+if (window.innerWidth >= (window.innerHeight / 10) * 16) {
+  lastWidth = (window.innerHeight / 10) * 16;
+  lastHeight = window.innerHeight * 0.67;
+} else {
+  lastWidth = window.innerWidth;
+  lastHeight = (window.innerWidth / 16) * 10 * 0.67;
+}
+
+function canvasSetup() {
+  if (window.innerWidth >= (window.innerHeight / 10) * 16) {
+    width = (window.innerHeight / 10) * 16;
+    height = window.innerHeight * 0.67;
+  } else {
+    width = window.innerWidth;
+    height = (window.innerWidth / 16) * 10 * 0.67;
+  }
+  canvas.width = width;
+  canvas.height = height;
+  hud.setAttribute(
+    "style",
+    `width: ${canvas.width}px; height: ${(canvas.height / 67) *
+      33}px;`
+  );
+  // hud.setAttribute("style", ``);
+  if (lastWidth !== canvas.width) {
+    // Adjust player position, based on degree of screen resize
+    player.x = Math.round((player.x * canvas.width) / lastWidth);
+    player.y = Math.round((player.y * canvas.height) / lastHeight);
+    lastWidth = canvas.width;
+    lastHeight = canvas.height;
+  }
+  return {
+    width: window.innerWidth,
+    height: window.innerHeight * 0.67,
+  };
+}
+
+function physicsSetup() {
+  friction = 0.8;
+  gravity = canvas.width * 0.0003;
+}
+
+function levelSetup() {
+  boxes = [];
+
+  function box(x, y, w, h, c, e = null) {
+    return {
+      x: Math.round((width * x) / 100),
+      y: Math.round((height * y) / 100),
+      width: Math.round((width * w) / 100),
+      height: Math.round((height * h) / 100),
+      color: c,
+      effect: e,
+    };
+  }
+
+  switch (level) {
+    case 0:
+      boxes.push(box(0, 0, 1.5, 100, "black"));
+      boxes.push(box(0, 95, 100, 5, "black"));
+      boxes.push(box(97.5, 0, 2.5, 37.5, "black"));
+      boxes.push(box(97.5, 65, 2.5, 42.5, "black"));
+      boxes.push(box(40, 85, 5, 5 / ((1 / 1.6) * 0.67), "black"));
+      boxes.push(box(65, 85, 5, 5 / ((1 / 1.6) * 0.67), "black"));
+      boxes.push(box(85, 65, 37.5, 42.5, "black"));
+      boxes.push(box(80, 75, 37.5, 42.5, "black"));
+      triggers.push(
+        box(102.5, 37.5, 2.5, 27.5, "rgba(0, 0, 0, 0)", { level: 1 })
+      );
+      break;
+    case 1:
+      player.x = 0;
+      boxes.push(box(0, 0, 2.5, 37.5, "black"));
+      boxes.push(box(0, 65, 2.5, 42.5, "black"));
+      boxes.push(box(0, 95, 100, 5, "black"));
+  }
+}
+function playerSetup() {
   player = {
-    x: width / 10,
-    y: 10,
-    width: 60,
-    height: 100,
-    speed: 4,
+    ...player,
+    width: canvas.width * 0.04,
+    height: canvas.height * 0.2,
+    speed: canvas.width * 0.003,
     velX: 0,
     velY: 0,
     jumping: false,
     grounded: false,
     color: "#E6AC27",
-  },
-  keys = [],
-  friction = 0.8,
-  gravity = 0.4,
-  boxes = [];
+  };
+}
 
-// dimensions
-boxes.push({
-  //box on left
-  x: -10,
-  y: 0,
-  width: 10,
-  height: height,
-  color: "black",
-});
-boxes.push({
-  //box for the ground
-  x: 0,
-  y: height - 10,
-  width: width,
-  height: 50,
-  color: "black",
-});
-boxes.push({
-  //box on right
-  x: width,
-  y: 0,
-  width: 10,
-  height: height,
-  color: "black",
-});
-
-canvas.width = width;
-canvas.height = height;
+canvasSetup();
+physicsSetup();
+levelSetup();
+playerSetup();
 
 function update() {
   // check keys
@@ -67,19 +135,19 @@ function update() {
     if (!player.jumping && player.grounded) {
       player.jumping = true;
       player.grounded = false;
-      player.velY = -player.speed * 2.5; //how high to jump
+      player.velY = -player.speed * 2; //how high to jump
     }
   }
   if (keys[39] || keys[68]) {
     // right arrow
     if (player.velX < player.speed) {
-      player.velX++;
+      player.velX += canvas.width * 0.001;
     }
   }
   if (keys[37] || keys[65]) {
     // left arrow
     if (player.velX > -player.speed) {
-      player.velX--;
+      player.velX -= canvas.width * 0.001;
     }
   }
 
@@ -90,10 +158,16 @@ function update() {
   ctx.beginPath();
 
   player.grounded = false;
+
+  // Draw boxes
   for (var i = 0; i < boxes.length; i++) {
-    //print boxes
     ctx.fillStyle = boxes[i].color;
-    ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+    ctx.fillRect(
+      boxes[i].x,
+      boxes[i].y,
+      boxes[i].width,
+      boxes[i].height
+    );
 
     var dir = colCheck(player, boxes[i]);
 
@@ -115,9 +189,23 @@ function update() {
   player.x += player.velX;
   player.y += player.velY;
 
-  ctx.fill(); //Draw charater stuff
+  // Player
   ctx.fillStyle = player.color;
   ctx.fillRect(player.x, player.y, player.width, player.height);
+
+  // Check for trigger collisions
+  for (var j = 0; j < triggers.length; j++) {
+    if (colCheck(player, triggers[j]) !== null) {
+      Object.keys(triggers[j].effect).forEach((key, index) => {
+        switch (key) {
+          case "level":
+            level = triggers[j].effect[key];
+            levelSetup();
+            break;
+        }
+      });
+    }
+  }
 
   requestAnimationFrame(update);
 }
@@ -129,8 +217,8 @@ function colCheck(shapeA, shapeB) {
     vY =
       shapeA.y + shapeA.height / 2 - (shapeB.y + shapeB.height / 2),
     // add the half widths and half heights of the objects
-    hWidths = shapeA.width / 2 + shapeB.width / 2,
-    hHeights = shapeA.height / 2 + shapeB.height / 2,
+    hWidths = (shapeA.width + shapeB.width) / 2,
+    hHeights = (shapeA.height + shapeB.height) / 2,
     colDir = null;
 
   // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
@@ -141,31 +229,36 @@ function colCheck(shapeA, shapeB) {
     if (oX >= oY) {
       if (vY > 0) {
         colDir = "t";
-        shapeA.y += oY;
+        shapeA.y += Math.ceil(oY * 10) / 10;
       } else {
         colDir = "b";
-        shapeA.y -= oY;
+        shapeA.y -= Math.ceil(oY * 10) / 10;
       }
     } else {
       if (vX > 0) {
         colDir = "l";
-        shapeA.x += oX;
+        shapeA.x += Math.ceil(oX * 10) / 10;
       } else {
         colDir = "r";
-        shapeA.x -= oX;
+        shapeA.x -= Math.ceil(oX * 10) / 10;
       }
     }
   }
   return colDir;
 }
 
-document.body.addEventListener("keydown", function(e) {
+window.addEventListener("keydown", function(e) {
   keys[e.keyCode] = true;
 });
 
-document.body.addEventListener("keyup", function(e) {
+window.addEventListener("keyup", function(e) {
   keys[e.keyCode] = false;
 });
+
+window.addEventListener("resize", canvasSetup);
+window.addEventListener("resize", physicsSetup);
+window.addEventListener("resize", levelSetup);
+window.addEventListener("resize", playerSetup);
 
 window.addEventListener("load", function() {
   update();
